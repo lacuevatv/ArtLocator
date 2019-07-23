e4d07dac074e859f724826d4ea3ef8633246a33b
coco <coco@lacueva.tv>
2019-07-23 15:37:12 -0300

    le hice un smoth scroll

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -421,9 +421,10 @@ function openMore (e, id) {
     
     //va hacia el contenedor y lo ubica en la ventana
     if (window.innerWidth > 960 ) {
-        var _top = document.querySelector('.map-section').scrollHeight + document.querySelector('.map-section').offsetTop;
-        window.scrollTo(0, _top);
-    _top
+        //var _top = document.querySelector('.map-section').scrollHeight + document.querySelector('.map-section').offsetTop;
+        //window.scrollTo(0, _top);
+        smoothScroll('.map-section');
+    }
 
     
     //asigna el tamaño al contenedor interno para que haga la animacion
@@ -998,14 +999,50 @@ function sendMsjNavigation(msj) {
 }
 
 
-//calcula posicion de un elemento
-function getOffset( el ) {
-    var _x = 0;
-    var _y = 0;
-    while( el && !isNaN( el.offsetLeft ) && !isNaN( el.offsetTop ) ) {
-        _x += el.offsetLeft - el.scrollLeft;
-        _y += el.offsetTop - el.scrollTop;
-        el = el.offsetParent;
+
+
+function elmYPosition(eID) {
+    var elm = document.querySelector(eID);
+    var y = elm.offsetTop;
+    var node = elm;
+    while (node.offsetParent && node.offsetParent != document.body) {
+        node = node.offsetParent;
+        y += node.offsetTop;
     }
-    return { top: _y, left: _x };
+    return y;
+}
+
+function currentYPosition() {
+    // Firefox, Chrome, Opera, Safari
+    if (self.pageYOffset) return self.pageYOffset;
+    // Internet Explorer 6 - standards mode
+    if (document.documentElement && document.documentElement.scrollTop)
+        return document.documentElement.scrollTop;
+    // Internet Explorer 6, 7 and 8
+    if (document.body.scrollTop) return document.body.scrollTop;
+    return 0;
+}
+
+function smoothScroll(eID) {
+    var startY = currentYPosition();
+    var stopY = elmYPosition(eID);
+    var distance = stopY > startY ? stopY - startY : startY - stopY;
+    if (distance < 100) {
+        scrollTo(0, stopY); return;
+    }
+    var speed = Math.round(distance / 100);
+    if (speed >= 20) speed = 20;
+    var step = Math.round(distance / 25);
+    var leapY = stopY > startY ? startY + step : startY - step;
+    var timer = 0;
+    if (stopY > startY) {
+        for ( var i=startY; i<stopY; i+=step ) {
+            setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+            leapY += step; if (leapY > stopY) leapY = stopY; timer++;
+        } return;
+    }
+    for ( var i=startY; i>stopY; i-=step ) {
+        setTimeout("window.scrollTo(0, "+leapY+")", timer * speed);
+        leapY -= step; if (leapY < stopY) leapY = stopY; timer++;
+    }
 }
