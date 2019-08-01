5da8983d4c49ba77dc0ff438c6a9e8e00722d559
coco <coco@lacueva.tv>
2019-08-01 19:21:01 -0300

    cambie los postperpag y arme una mini funcion para ir cargando en el serializer

--- public/inc/serializer.php
+++ public/inc/serializer.php
@@ -17,15 +17,76 @@ var_dump($locations);
 /*$kiosko = getLocation(4);
 var_dump($kiosko);*/
 
-$ar= array('sanjusto-despues.jpg', 'sanjusto-despues@2x.jpg' );
+$imagen= array('chascomus-despues.jpg' );
 
-echo serialize($ar);
+echo serialize($imagen);
 
 echo '<br><br>';
 
-$ars= array(
-    'antes' => array( 'sanjusto-antes.jpg', 'sanjusto-antes@2x.jpg' ),
-    'despues' => array( 'sanjusto-despues.jpg', 'sanjusto-despues@2x.jpg' ),
+$imagenes= array(
+    'antes' => array( 'chascomus-antes.jpg' ),
+    'despues' => array( 'chascomus-despues.jpg' ),
 );
 
-echo serialize($ars);
+echo serialize($imagenes);
+
+echo '<br><br>';
+
+$video = '';
+
+echo serialize($video);
+
+echo '<br><br>';
+
+$kiosko = array(
+    'titulo' => 'Ruartes Julio Javier',
+    'latitud' => '-35.5768292',
+    'longitud' => '-58.0087402',
+    'ubicacion_id' => 2,
+    'marker' => 'marker-amarillo.png',
+    'likes' => 0,
+    'data_titulo' => 'Plantas de Chascomús',
+    'data_imagen' => serialize($imagen),
+    'data_tag' => 'Pedro Arteaga (El Pit)',
+    'data_resumen' => 'Una combinación de colores y formas naturales que crean un espacio atractivo para el ambiente callejero.',
+    'data_video' => '',
+    'data_texto' => 'Una combinación de colores y formas naturales que crean un espacio atractivo para el ambiente callejero.',
+    'data_direccion' => 'Avda Lastra 500 – CHASCOMUS',
+    'data_imagenes' => serialize($imagenes),
+    'fecha' => '2019-08-01',
+);
+
+$query = "INSERT into kioskos (titulo, latitud, longitud, ubicacion_id, marker, likes, data_titulo, data_imagen, data_tag, data_resumen, data_video, data_texto, data_direccion, data_imagenes, fecha) values(";
+
+foreach ($kiosko as $key => $value) {
+    
+    $query .= "'" .$kiosko[$key] . "'";
+    if ($key != 'fecha') {
+        $query .= ", ";
+    }
+    
+}
+
+$query .= ")";
+
+echo $query;
+
+
+/*$kiosko = array(
+    'titulo' => '',
+    'latitud' => '',
+    'longitud' => '',
+    'ubicacion_id' => 2,
+    'marker' => '',
+    'likes' => 0,
+    'data_titulo' => '',
+    'data_imagen' => serialize($imagen),
+    'data_tag' => '',
+    'data_resumen' => '',
+    'data_popup' => 1,
+    'data_video' => serialize($video),
+    'data_texto' => '',
+    'data_direccion' => '',
+    'data_imagenes' => serialize($imagenes),
+    'fecha' => '2019-08-01',
+);*/
